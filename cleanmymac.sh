055fe6b51eab0d23fb45fb3bfde4e1cc94e6ebe1
wangmh <wangmh@tuya.com>
2024-10-24 09:54:42 +0800

    feat: 使用ChatGPT优化脚本

--- cleanmymac.sh
+++ cleanmymac.sh
@@ -7,134 +7,112 @@ set -e
 # set -x
 
 displayNotification() {
-    description="${1}"
-	title="${2}"
+    local description="${1}"
+    local title="${2}"
     echo "$description - $title"
-    # osascript -e "display notification \"$description\" with title "\"$title\""
-osascript << EOF
-    display notification "\"$description\"" with title "\"$title\""
+    osascript << EOF
+    display notification "$description" with title "$title"
 EOF
 }
 
 bytesToHumanReadable() {
-    bytes=`echo "${1}" | numfmt --to=iec`
-    result="$bytes of space was cleaned up :3"
-    noti="来自 CleanMyMac"
+    local bytes
+    bytes=$(numfmt --to=iec <<< "${1}")
+    local result="$bytes of space was cleaned up :3"
+    local noti="来自 CleanMyMac"
     displayNotification "$result" "$noti"
 }
 
-available () {
-    echo `df / | tail -1 | awk '{print $4}'`
+available() {
+    df / | awk 'NR==2 {print $4}'
 }
 
-####
-updateBrew () {
-    echo "update brew"
+updateBrew() {
+    echo "Updating Homebrew..."
     arch -arm64 brew update && arch -arm64 brew upgrade && arch -arm64 brew cu -a -y
 }
 
 updateMas() {
-    echo "update mas"
+    echo "Updating Mac App Store..."
     mas upgrade
 }
 
 clearCocoapods() {
-    echo "clean cocoapods"
+    echo "Cleaning CocoaPods..."
     pod cache clean --all
 }
 
 clearXcode() {
+    echo "Cleaning Xcode..."
     xcrun simctl delete unavailable
     rm -rf ~/Library/Developer/Xcode/DerivedData/* &>/dev/null
     rm -rf ~/Library/Developer/Xcode/Archives/* &>/dev/null
-    # rm -rf ~/Library/Developer/CoreSimulator/Devices/* &>/dev/null
     rm -rf ~/Library/Developer/Xcode/Products/* &>/dev/null
 
-    cd ~/Library/Developer/Xcode/iOS\ DeviceSupport
-    ifile=`ls | sort -rV | head -n1`
-    ls | grep -v "${ifile}" | tr "\n" "\0" | xargs -0 rm -rf
+    cd ~/Library/Developer/Xcode/iOS\ DeviceSupport || return
+    local ifile
+    ifile=$(ls | sort -rV | head -n1)
+    ls | grep -v "${ifile}" | xargs -I {} rm -rf "{}"
 }
 
-####
-display () {
-    effect="${1}"
-    text="${2}"
-    detailDes="Do you wish to $effect $text software?"
-osascript << EOF
+displayDialog() {
+    local effect="${1}"
+    local text="${2}"
+    local detailDes="Do you wish to ${effect} ${text} software?"
+    osascript << EOF
     display dialog "$detailDes" buttons {"Yes", "No"} default button "No"
 EOF
 }
 
-displayDialog () {
-    res=$(display "${1}" "${2}")
-    # echo $res
-    if [ "$res" = "button returned:Yes" ]; then
-        echo true
-    else
-        echo false
-    fi
+shouldProceed() {
+    local action="${1}"
+    local name="${2}"
+    local res
+    res=$(displayDialog "$action" "$name")
+    [[ "$res" == "button returned:Yes" ]]
 }
 
-displayUpdate () {
-    bool=$(displayDialog "update" "${1}")
-    if [[ "$bool" == true ]]; then
-        echo true
+main() {
+    ############################## Update
+    if shouldProceed "update" "brew"; then
+        updateBrew
     else
-        echo false
+        echo "Keeping brew"
     fi
+
+    if shouldProceed "update" "mas"; then
+        updateMas
+    else
+        echo "Keeping mas"
+    fi
+
+    ############################## Clear
+    oldAvailable=$(available)
+
+    if shouldProceed "clear" "cocoapods"; then
+        clearCocoapods
+    else
+        echo "Don't clean up CocoaPods"
+    fi
+
+    # Clean Xcode
+    clearXcode
+
+    # Clean up Homebrew
+    echo "Cleaning up Homebrew..."
+    arch -arm64 brew cleanup
+
+    # Clean up Ruby
+    echo "Cleaning up Ruby..."
+    gem cleanup
+
+    clear && echo 'Success!'
+
+    newAvailable=$(available)
+    count=$((newAvailable - oldAvailable))
+    count=$((count * 512))
+    bytesToHumanReadable "$count"
 }
 
-displayClear () {
-    bool=$(displayDialog "clear" "${1}")
-    if [[ "$bool" == true ]]; then
-        echo true
-    else
-        echo false
-    fi
-}
-
-
-##############################update
-boolBrew=$(displayUpdate "brew")
-if [[ "$boolBrew" == true ]]; then
-    updateBrew
-else
-    echo "keep brew"
-fi
-
-boolMas=$(displayUpdate "mas")
-if [[ "$boolMas" == true ]]; then
-    updateMas
-else
-    echo "keep mas"
-fi
-
-##############################clear
-# <--------------------------
-oldAvailable=$(available)
-
-boolCocoapods=$(displayClear "cocoapods")
-if [[ "$boolCocoapods" == true ]]; then
-    clearCocoapods
-else
-    echo "Don't clean up cocoapods"
-fi
-
-# xcode
-echo 'Cleanup Xcode'
-clearXcode
-
-# Cleaning Up Homebrew.
-arch -arm64 brew cleanup
-
-#Cleaning Up Ruby.
-printf "Cleanup up Ruby.\n"
-gem cleanup
-
-clear && echo 'Success!'
-
-newAvailable=$(available)
-# -------------------------->
-count=$((newAvailable-oldAvailable))
-count=$(( $count * 512))
-bytesToHumanReadable $count
+# Call the main function to execute the script
+main
